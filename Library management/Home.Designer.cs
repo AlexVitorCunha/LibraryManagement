ced75a8e993e3a19b7babee75957bd966986b8de
Lionel Pineda <85717297+Snackraptor@users.noreply.github.com>
2021-08-14 13:24:15 -0400

    Added import export and style to home

--- Library management/Home.Designer.cs
+++ Library management/Home.Designer.cs
@@ -37,55 +37,63 @@ namespace Library_management
             this.btnAddBook = new System.Windows.Forms.Button();
             this.btnSearch = new System.Windows.Forms.Button();
             this.searchType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.bookList)).BeginInit();
             this.SuspendLayout();
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(451, 8);
+            this.btnLogout.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnLogout.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnLogout.Location = new System.Drawing.Point(712, 9);
             this.btnLogout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogout.Name = "btnLogout";
             this.btnLogout.Size = new System.Drawing.Size(67, 25);
             this.btnLogout.TabIndex = 0;
             this.btnLogout.Text = "Logout";
-            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.UseVisualStyleBackColor = false;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
             // bookList
             // 
             this.bookList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.bookList.Location = new System.Drawing.Point(27, 125);
+            this.bookList.Location = new System.Drawing.Point(23, 177);
             this.bookList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bookList.Name = "bookList";
             this.bookList.RowHeadersWidth = 62;
             this.bookList.RowTemplate.Height = 28;
-            this.bookList.Size = new System.Drawing.Size(481, 147);
+            this.bookList.Size = new System.Drawing.Size(756, 224);
             this.bookList.TabIndex = 1;
             this.bookList.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.bookList_CellContenDoubleClick);
             this.bookList.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.bookList_CellContenDoubleClick);
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(119, 56);
+            this.txtSearch.BackColor = System.Drawing.Color.SkyBlue;
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSearch.Location = new System.Drawing.Point(82, 144);
             this.txtSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(68, 20);
+            this.txtSearch.Size = new System.Drawing.Size(229, 20);
             this.txtSearch.TabIndex = 2;
             // 
             // lblSearch
             // 
             this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSearch.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lblSearch.Location = new System.Drawing.Point(38, 60);
+            this.lblSearch.Location = new System.Drawing.Point(18, 144);
             this.lblSearch.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(41, 13);
+            this.lblSearch.Size = new System.Drawing.Size(60, 29);
             this.lblSearch.TabIndex = 3;
             this.lblSearch.Text = "Search";
             // 
             // clear
             // 
-            this.clear.Location = new System.Drawing.Point(458, 103);
+            this.clear.Location = new System.Drawing.Point(524, 143);
             this.clear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.clear.Name = "clear";
             this.clear.Size = new System.Drawing.Size(50, 19);
@@ -95,7 +103,7 @@ namespace Library_management
             // 
             // btnAddBook
             // 
-            this.btnAddBook.Location = new System.Drawing.Point(347, 103);
+            this.btnAddBook.Location = new System.Drawing.Point(703, 154);
             this.btnAddBook.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAddBook.Name = "btnAddBook";
             this.btnAddBook.Size = new System.Drawing.Size(76, 19);
@@ -106,7 +114,7 @@ namespace Library_management
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(415, 54);
+            this.btnSearch.Location = new System.Drawing.Point(458, 143);
             this.btnSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(62, 19);
@@ -125,18 +133,50 @@ namespace Library_management
             "Author",
             "Genre",
             "Year"});
-            this.searchType.Location = new System.Drawing.Point(271, 54);
+            this.searchType.Location = new System.Drawing.Point(330, 143);
             this.searchType.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.searchType.Name = "searchType";
             this.searchType.Size = new System.Drawing.Size(116, 21);
             this.searchType.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Myanmar Text", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 48);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Library Dashboard";
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(23, 408);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(211, 23);
+            this.btnImport.TabIndex = 9;
+            this.btnImport.Text = "Import Data";
+            this.btnImport.UseVisualStyleBackColor = true;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(558, 406);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(220, 23);
+            this.btnExport.TabIndex = 10;
+            this.btnExport.Text = "Export to Excel";
+            this.btnExport.UseVisualStyleBackColor = true;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(50)))), ((int)(((byte)(70)))));
-            this.ClientSize = new System.Drawing.Size(533, 306);
+            this.ClientSize = new System.Drawing.Size(790, 443);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.searchType);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.btnAddBook);
@@ -165,5 +205,8 @@ namespace Library_management
         private System.Windows.Forms.Button btnAddBook;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.ComboBox searchType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnExport;
     }
 }
