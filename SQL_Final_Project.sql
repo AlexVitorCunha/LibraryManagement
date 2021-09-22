ade7b5ffb342a6b9df18290bbaa02846b999577c
AlexVitorCunha <62349585+AlexVitorCunha@users.noreply.github.com>
2021-09-22 14:25:05 -0300

    update sql query

--- SQL_Final_Project.sql
+++ SQL_Final_Project.sql
@@ -1,3 +1,8 @@
+/*
+This query is needed to create all the tables used on the LibraryManagement Windows Application
+*/
+
+
 CREATE TABLE [credentials]( 
 [id] [int] IDENTITY(1,1)  PRIMARY KEY, 
 [username] [nvarchar](50) NOT NULL, 
@@ -12,6 +17,7 @@ CREATE TABLE [books](
 [genre][nvarchar](50) NOT NULL,
 [year][int],
 [quantity][smallint] NOT NULL,
+book_cover [nvarchar](100) NULL
 );
 
 CREATE TABLE [books_borrowed](
